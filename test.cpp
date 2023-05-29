a3be56100362e70bf55c1c7d8b97f140fcf9af13
xxhsu <xxhsu@live.cn>
2023-05-29 19:34:40 +0000

    Print puzzle

--- test.cpp
+++ test.cpp
@@ -4,19 +4,33 @@
 
 using namespace std;
 
+void printPuzzle(char puzzle[3][3]) {
+   for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+         cout << puzzle[i][j];
+      }
+      cout << endl;
+   }
+}
+
 int main() {
-   string puzzle;
-   string line;
+   char puzzle[3][3];
 
    ifstream infile;
    infile.open(".puzzle");
 
    if (infile.is_open()) {
+      string line;
       cout << "Loading puzzle..." << endl;
-      while (getline(infile, line)) {
-         cout << line << endl;
+
+      for (int i = 0; i < 3; i++) {
+         getline(infile, line);
+         for (int j = 0; j < 3; j++) {
+            puzzle[i][j] = line[j];
+         }
       }
       infile.close();
+      printPuzzle(puzzle);
    } else {
       cout << "Puzzle file not found" << endl;
    }
