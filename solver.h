adeb2f7a6e9ba6c947363a000faf35f062fdbd84
xxhsu <xxhsu@live.cn>
2023-07-19 02:52:13 +0800

    Remove indent

--- solver.h
+++ solver.h
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-	using Puzzle = vector<vector <unsigned short>>;
-	using PuzzleSet = vector<Puzzle>;
-	using PuzzlePiece = enum {ZERO = 0, ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8};
+using Puzzle = vector<vector <unsigned short>>;
+using PuzzleSet = vector<Puzzle>;
+using PuzzlePiece = enum {ZERO = 0, ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8};
 
 class Solver
 {
